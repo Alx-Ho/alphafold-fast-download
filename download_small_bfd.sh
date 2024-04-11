278bcaa52d5a7f69e8870f372091484c10f4a86e
Alex Ho <aho@aho-MacBook-Air.local>
2024-04-11 10:45:28 -0500

    pig for small bfd

--- download_small_bfd.sh
+++ download_small_bfd.sh
@@ -29,6 +29,11 @@ if ! command -v aria2c &> /dev/null ; then
     exit 1
 fi
 
+if ! command -v pigz &> /dev/null ; then
+    echo "Error: pigz could not be found. Please install pigz (sudo apt install pigz)."
+    exit 1
+fi
+
 DOWNLOAD_DIR="$1"
 ROOT_DIR="${DOWNLOAD_DIR}/small_bfd"
 SOURCE_URL="https://storage.googleapis.com/alphafold-databases/reduced_dbs/bfd-first_non_consensus_sequences.fasta.gz"
@@ -37,5 +42,5 @@ BASENAME=$(basename "${SOURCE_URL}")
 mkdir --parents "${ROOT_DIR}"
 aria2c "${SOURCE_URL}" --dir="${ROOT_DIR}"
 pushd "${ROOT_DIR}"
-gunzip "${ROOT_DIR}/${BASENAME}"
+pigz -dc "${BASENAME}" > "${BASENAME%.gz}"
 popd
